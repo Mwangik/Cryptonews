27e029f9e9a11619e8b27540969bcb727c4f2e8a
Mwangik <gkmwangi0@gmail.com>
2019-04-03 17:44:29 +0300

    connect to cryptocompare api

--- app/crypto/views.py
+++ app/crypto/views.py
@@ -1,5 +1,11 @@
+import requests
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    context = {'api': api}
+    return render(request, 'index.html', context)
