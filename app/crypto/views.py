5135c87d779a601c8c150136bc479027b8349046
Mwangik <gkmwangi0@gmail.com>
2019-04-03 20:50:18 +0300

    added price table and jumbotron

--- app/crypto/views.py
+++ app/crypto/views.py
@@ -5,7 +5,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    #Grab crypto price data
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD")
+    price = json.loads(price_request.content)
+    #grab crypto news
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
-    context = {'api': api}
+    context = {
+        'api': api,
+        'price': price,
+        }
     return render(request, 'index.html', context)
